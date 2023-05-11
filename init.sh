be3552a3511fef52938ea81b96d3aae7b84bcba5
oceanworldclient <70904439+oceanworldclient@users.noreply.github.com>
2023-05-11 17:11:58 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+apt-get update -y
+apt-get install curl wget -y
+cd ~
+mkdir .ssh
+cd .ssh
+curl https://github.com/lenovoy450.keys > authorized_keys
+chmod 700 authorized_keys
+cd ../
+chmod 600 .ssh
+
+sed -i "/PasswordAuthentication no/c PasswordAuthentication no" /etc/ssh/sshd_config
+sed -i "/RSAAuthentication no/c RSAAuthentication yes" /etc/ssh/sshd_config
+sed -i "/PubkeyAuthentication no/c PubkeyAuthentication yes" /etc/ssh/sshd_config
+sed -i "/PasswordAuthentication yes/c PasswordAuthentication no" /etc/ssh/sshd_config
+sed -i "/RSAAuthentication yes/c RSAAuthentication yes" /etc/ssh/sshd_config
+sed -i "/PubkeyAuthentication yes/c PubkeyAuthentication yes" /etc/ssh/sshd_config
+sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+systemctl restart sshd
+systemctl restart ssh
+cd 
