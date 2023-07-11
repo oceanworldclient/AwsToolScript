3907f4d9859c4f1246c19f3afe555c8d67f37109
oceanworldclient <70904439+oceanworldclient@users.noreply.github.com>
2023-07-11 10:41:04 +0800

    Create install_report.sh

---
+++ install_report.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ ! -d /root/report ];then
+    mkdir /root/report
+fi
+cd /root/report
+if [ -f /root/report/report ];then
+    rm /root/report/report
+fi
+wget https://raw.githubusercontent.com/oceanworldclient/AwsToolScript/main/report
+chmod +x /root/report/report
+if [ ! -f /root/report/report.lock ];then
+    touch /root/report/report.lock
+fi
+
+if [ -f /var/spool/cron/crontabs/root ];then
+    v=$(cat /var/spool/cron/crontabs/root)
+    if [[ "$v" == *"report.lock"* ]];then
+        v=""
+    else
+        echo "0 */1 * * * flock -xn   /root/report/report.lock   -c '/root/report/report -api https://panel-node.19900101.xyz/ServerAlive/Health -web ocean -ip 127.0.0.1 >> /dev/null 2>&1'" >> /var/spool/cron/crontabs/root
+    fi
+elif [ -f /var/spool/cron/root ];then
+    v=$(cat /var/spool/cron/crontabs/root)
+    if [[ "$v" == *"report.lock"* ]];then
+        v=""
+    else
+        echo "0 */1 * * * flock -xn   /root/report/report.lock   -c '/root/report/report -api https://panel-node.19900101.xyz/ServerAlive/Health -web ocean -ip 127.0.0.1 >> /dev/null 2>&1'" >> /var/spool/cron/root
+    fi   
+else
+    echo "No"
+fi
+
+systemctl restart cron
